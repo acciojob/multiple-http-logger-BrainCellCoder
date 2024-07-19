f428d85c400c04e52785176ce9073884b5fc7fed
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-19 11:15:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const urls = [
   "https://jsonplaceholder.typicode.com/posts/2",
   "https://jsonplaceholder.typicode.com/posts/3",
 ];
-
-// Don't change the above line
-// Write your code here
+async function fetchAndLog(urls) {
+  try {
+    const fetchPromises = urls.map(url => fetch(url).then(response => response.json()));
+    const results = await Promise.all(fetchPromises);
+    results.forEach((result, index) => {
+      console.log(`Response from ${urls[index]}:`, result);
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+fetchAndLog(urls);
